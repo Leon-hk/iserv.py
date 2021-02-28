e6c17a341d1b3daf2d1d50fb5c2dce456335719e
CaptainSword123 <37672722+CaptainSword123@users.noreply.github.com>
2021-02-28 23:54:35 +0100

    Lite Klassen und User Klasse

--- test/test.py
+++ test/test.py
@@ -19,9 +19,14 @@ import iserv
 
 client = iserv.Client(url, username, pw)
 
-task = client.get_task(4060)
+task = client.get_task(4339)
 print(task.title)
 print(task.description)
+print(task.teacher.name)
+print(task.teacher.firstname)
+print(task.teacher.lastname)
+print(task.teacher.username)
+print(task.teacher.mail)
 print(task.tags)
 print(task.start)
 print(task.end)
